77ca0993837c8986474fe8d44d48ab0c37a8c5ff
JOHN NELSON <JohnNelson4850@gmail.com>
2019-07-11 21:11:20 -0700

    eod

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,18 @@
+import isEven from '../is-even.js';
 const test = QUnit.test;
 
-test('time to test!', function(assert) {
+test('checks if even', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const x = 4;
+    
     //Act 
+    const expected = isEven(x);
+    assert.equal(expected, true);
+
     // Call the function you're testing and set the result to a const
 
     //Assert
     // What should the result be?
-    assert.equal(true, false);
+    assert.equal(expected, false);
 });
